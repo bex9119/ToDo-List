6ff534d20f07694907db43d7120676e77fb3c14d
bex9119 <60200396+bex9119@users.noreply.github.com>
2024-01-26 12:11:40 +0000

    add refresh list button

--- src/Components/TodoBody.tsx
+++ src/Components/TodoBody.tsx
@@ -1,15 +1,17 @@
 import { useState} from "react"
 import Input from "./Input"
 import List from "./List"
+import Refresh from "./Refresh"
 
 const TodoBody = () => {
-    const [toDoList, setToDoList] = useState<string[]>(['test'])
+    const [toDoList, setToDoList] = useState<string[]>([''])
 
 
     return (
       <>
         <List toDoList={toDoList} />
         <Input setToDoList={setToDoList} />
+        <Refresh setToDoList={setToDoList} />
       </>
     );
 }
